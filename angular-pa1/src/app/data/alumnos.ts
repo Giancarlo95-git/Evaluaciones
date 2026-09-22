9b398fc85b78cc9b386acb3ee4e24d1af8491593
Giancarlo95gh <gianqq.pe.95@gmail.com>
2026-09-21 19:21:28 -0500

    Actualiza busqueda de solicitudes

--- angular-pa1/src/app/data/alumnos.ts
+++ angular-pa1/src/app/data/alumnos.ts
@@ -8,13 +8,17 @@ export const alumnos: Alumno[] = [
     nombreCompleto: 'Giancarlo Quispe',
     correoInstitucional: '72478618@mail.isil.pe',
     carrera: 'Desarrollo de Software',
-    ciclo: 4
+    ciclo: 5
   },
   {
     codigo: 8,
     nombreCompleto: 'Pepe Lucho',
     correoInstitucional: '74747474@mail.isil.pe',
     carrera: 'Desarrollo de Software',
-    ciclo: 4
+    ciclo: 3
   }
 ];
+
+export const solicitudAlumnoCodigo = (codigo: number): Alumno | undefined =>
+  alumnos.find(alumno => alumno.codigo === codigo);
+console.log(solicitudAlumnoCodigo(8)) // impresión en consola
